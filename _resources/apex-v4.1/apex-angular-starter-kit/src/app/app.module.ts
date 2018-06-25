2869a6f981a7dec2167261f50580f81c90d2d55d
William Mansfield <wmansfield@socialhaven.com>
2018-06-25 14:51:20 -0400

    Raw Include of Template

---
+++ _resources/apex-v4.1/apex-angular-starter-kit/src/app/app.module.ts
@@ -0,0 +1,38 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from "./shared/shared.module";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { AppComponent } from './app.component';
+import { ContentLayoutComponent } from "./layouts/content/content-layout.component";
+import { FullLayoutComponent } from "./layouts/full/full-layout.component";
+
+import { AuthService } from './shared/auth/auth.service';
+import { AuthGuard } from './shared/auth/auth-guard.service';
+
+import * as $ from 'jquery';
+
+
+
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        FullLayoutComponent,
+        ContentLayoutComponent
+    ],
+    imports: [
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        SharedModule,
+        NgbModule.forRoot()
+    ],
+    providers: [
+        AuthService,
+        AuthGuard
+    ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
